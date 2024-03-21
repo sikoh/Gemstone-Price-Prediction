24230b594a99393ea6ddf44b3aead45ceae6cbf6
Silvana K <silvanakoharian@gmail.com>
2024-03-21 05:04:30 -0400

    create components directory

--- src/gemstone_price_prediction/components/data_ingestion.py
+++ src/gemstone_price_prediction/components/data_ingestion.py
@@ -5,3 +5,6 @@ from src.gemstone_price_prediction.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+# from src.gemstone_price_prediction.components.data_transformation
+# from src.gemstone_price_prediction.components.model_trainer
