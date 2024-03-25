ea134309ae165a9f74617d35aa8befc896f04f36
Silvana K <silvanakoharian@gmail.com>
2024-03-25 19:46:16 -0400

    create print_evaluated_results func

--- src/gemstone_price_prediction/utils.py
+++ src/gemstone_price_prediction/utils.py
@@ -50,3 +50,30 @@ def model_metrics(true, predicted):
     except Exception as e:
         logging.info('Exception Occured while evaluating metric')
         raise CustomException(e,sys)
+    
+
+def print_evaluated_results(Xtrain,ytrain,Xtest,ytest,model):
+    try:
+        y_train_pred = model.predict(Xtrain)
+        y_test_pred = model.predict(Xtest)
+
+        # Evaluate Train and Test dataset
+        model_train_mae , model_train_rmse, model_train_r2 = model_metrics(ytrain, y_train_pred)
+        model_test_mae , model_test_rmse, model_test_r2 = model_metrics(ytest, y_test_pred)
+
+        # Printing results
+        print('Model performance for Training set')
+        print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
+        print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+        print("- R2 Score: {:.4f}".format(model_train_r2))
+
+        print('----------------------------------')
+    
+        print('Model performance for Test set')
+        print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+        print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+        print("- R2 Score: {:.4f}".format(model_test_r2))
+    
+    except Exception as e:
+        logging.info('Exception occured during printing of evaluated results')
+        raise CustomException(e,sys)
