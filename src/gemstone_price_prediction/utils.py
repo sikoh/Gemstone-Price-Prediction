f4110cb4d9ccce77396907cff6a6d92f2d3a4e3b
Silvana K <silvanakoharian@gmail.com>
2024-03-25 19:33:54 -0400

    create evaluate_models func

--- src/gemstone_price_prediction/utils.py
+++ src/gemstone_price_prediction/utils.py
@@ -19,3 +19,22 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+    def evaluate_models(Xtrain, ytrain, Xtest, ytest, models):
+        try:
+            report = {}
+            for model_name, model in models.items():
+                # Train model
+                model.fit(Xtrain, ytrain)
+
+                # Predict Testing data
+                y_test_pred = model.predict(Xtest)
+
+                # Get R2 score for test data
+                test_model_score = r2_score(ytest, y_test_pred)
+                report[model_name] = test_model_score
+
+        except Exception as e:
+            logging.info('Exception occured during model training')
+            raise CustomException(e,sys)
