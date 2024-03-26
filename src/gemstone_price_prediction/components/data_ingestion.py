88c2b91af3f33889f5d2920025a621cd7d85e662
Silvana K <silvanakoharian@gmail.com>
2024-03-26 16:55:20 -0400

    add data_ingestion

--- src/gemstone_price_prediction/components/data_ingestion.py
+++ src/gemstone_price_prediction/components/data_ingestion.py
@@ -8,3 +8,51 @@ from dataclasses import dataclass
 
 # from src.gemstone_price_prediction.components.data_transformation
 # from src.gemstone_price_prediction.components.model_trainer
+
+
+# Initialize Data Ingestion Configuration
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+# Create a class for Data Ingestion
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Data ingestion methos started')
+        try:
+            df = pd.read_csv('notebook/data/gemstone.csv')
+            logging.info('Dataset read as pandas DataFrame')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+
+            logging.info('Train Test Split initiated')
+            train_set, test_set = train_test_split(df, test_size=0.2, random=42)
+
+            logging.info('Saving Train and Test data files')
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info('Ingestion of data Completed')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            logging.info('Exception occured at Data Ingestion stage')
+            raise CustomException(e, sys)
+        
+
+# Run Data ingestion
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initate_data_ingestion()
+
